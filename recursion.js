de79aa383041ebf834fb9001bf352755e692c12e
Fazlulkarim2 <mdfazlulk103@gmail.com>
2021-08-11 00:45:29 +0600

    for loop added

---
+++ recursion.js
@@ -0,0 +1,15 @@
+
+// let sum = 0;
+// for (let i = 0; i <= 6; i++) {
+//     // console.log(i);
+//     sum = sum + i;
+// }
+// console.log(sum);
+
+
+let sum = 0;
+for (let i = 6; i >= 0; i--) {
+    console.log(i);
+    sum = sum + i;
+}
+console.log(sum);
